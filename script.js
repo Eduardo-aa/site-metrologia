90cba74d27360db6dd627e778dacc0baee3a2c13
Eduardo.dev <eduardomaia02122006@gmail.com>
2024-12-18 00:46:18 -0300

    mudanças

---
+++ script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal(".Título", { rotate: { x: 0, y: 80, z: 0 } }, { duration: 1000 });
+sr.reveal(".Parágrafo", { duration: 1000 });
+sr.reveal(".Instrumentos", { duration: 1000 });
+sr.reveal(".img-maqui", { duration: 1000 });
+sr.reveal(".img-inst", { duration: 1000 });
+sr.reveal("#downloadBtn", { duration: 1000 });
+
+
